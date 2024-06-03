ee452256321b9186446ba84a0d211cb4c380ee2b
taufikdimas <taufikdimasedystara@gmail.com>
2024-06-03 15:50:40 +0700

    update: UAS Sisop

--- UAS-Taufik.sh
+++ UAS-Taufik.sh
@@ -1,125 +1,134 @@
 #!/bin/bash
 
-# Fungsi untuk menampilkan menu
 show_menu() {
-    echo "======= Menu ========"
-    echo "1. Word ke PDF"
-    echo "2. PDF ke Word"
-    echo "3. PPT ke PDF"
-    echo "4. PDF ke PPT"
-    echo "5. Excel ke PDF"
-    echo "6. JPG ke PDF"
-    echo "7. Gabungkan PDF"
-    echo "8. Pisahkan PDF"
-    echo "9. Kompress PDF"
-    echo "10. Kunci PDF"
-    echo "11. Buka kunci PDF"
-    echo "12. Ubah kunci PDF"
-    echo "0. Keluar"
+    echo "╔════════════════════════════════╗"
+    echo -e "║   ${BLUE}    Pilih Menu TransPDF   ${NC}  ║"
+    echo "╠════════════════════════════════╣"
+    echo "║1.  Word ke PDF                 ║"
+    echo "║2.  PDF ke Word                 ║"
+    echo "║3.  PPT ke PDF                  ║"
+    echo "║4.  PDF ke PPT                  ║"
+    echo "║5.  Excel ke PDF                ║"
+    echo "║6.  JPG ke PDF                  ║"
+    echo "║7.  Gabungkan PDF               ║" 
+    echo "║8.  Pisahkan PDF                ║"
+    echo "║9.  Kompress PDF                ║"
+    echo "║10. Kunci PDF                   ║"
+    echo "║11. Buka kunci PDF              ║"
+    echo "║12. Ubah kunci PDF              ║"
+    echo "║0.  Keluar                      ║"
+    echo "╚════════════════════════════════╝"
 }
 
-# Fungsi untuk konversi dokumen menggunakan libreoffice
+# warna
+PINK='\033[38;5;205m'
+YELLOW='\033[38;5;226m'
+GREEN='\033[38;5;82m'
+RED='\033[38;5;196m'
+BLUE='\033[38;5;27m'
+NC='\033[0m' 
+
+# konversi dokumen menggunakan libreoffice
 convert_to_pdf() {
     input_file=$1
     output_format=$2
     libreoffice --headless --convert-to "$output_format" "$input_file"
     if [ $? -eq 0 ]; then
-        echo "Konversi berhasil: ${input_file} -> ${output_format}"
+        echo -e "${GREEN}Konversi berhasil: ${input_file}${NC} -> ${YELLOW}${output_format}${NC}"
     else
-        echo "Konversi gagal"
+        echo -e "${RED}Konversi gagal${NC}"
     fi
 }
 
-# Fungsi untuk konversi PDF ke Word menggunakan pdftotext dan pandoc
+# konversi PDF ke Word menggunakan pdftotext dan pandoc
 convert_pdf_to_word() {
     input_file=$1
     output_file=$2
     pdftotext "$input_file" temp.txt
     pandoc temp.txt -o "$output_file"
     if [ $? -eq 0 ]; then
-        echo "Konversi berhasil: ${input_file} -> ${output_file}"
+        echo -e "${GREEN}Konversi berhasil: ${input_file} -> ${output_file}${NC}"
     else
-        echo "Konversi gagal"
+        echo -e "${RED}Konversi gagal${NC}"
     fi
     rm temp.txt
 }
 
-# Fungsi untuk konversi PDF ke PPT menggunakan pdf2pptx
+# konversi PDF ke PPT menggunakan pdf2pptx
 convert_pdf_to_ppt() {
     input_file=$1
     output_file=$2
     pdf2pptx "$input_file" -o "$output_file"
     if [ $? -eq 0 ]; then
-        echo "Konversi berhasil: ${input_file} -> ${output_file}"
+        echo -e "${GREEN}Konversi berhasil: ${input_file} -> ${output_file}${NC}"
     else
-        echo "Konversi gagal"
+        echo -e "${RED}Konversi gagal${NC}"
     fi
 }
 
-# Fungsi untuk menggabungkan PDF menggunakan pdftk
+# menggabungkan PDF menggunakan pdftk
 merge_pdfs() {
     output_file=$1
     shift
     input_files=$@
     pdftk $input_files cat output "$output_file"
     if [ $? -eq 0 ]; then
-        echo "Penggabungan berhasil: $output_file"
+        echo -e "${GREEN}Penggabungan berhasil: $output_file${NC}"
     else
-        echo "Penggabungan gagal"
+        echo -e "${RED}Gagal menggabungkan PDF${NC}"
     fi
 }
 
-# Fungsi untuk memisahkan PDF menggunakan pdfseparate
+# memisahkan PDF menggunakan pdfseparate
 split_pdf() {
     input_file=$1
     output_pattern=$2
     pdfseparate "$input_file" "$output_pattern"
     if [ $? -eq 0 ]; then
-        echo "Pemisahan berhasil"
+        echo -e "${GREEN}Pemisahan berhasil${NC}"
     else
-        echo "Pemisahan gagal"
+        echo -e "${RED}Gagal Mempisahkan PDF${NC}"
     fi
 }
 
-# Fungsi untuk mengompres PDF menggunakan Ghostscript
+# mengompres PDF menggunakan Ghostscript
 compress_pdf() {
     input_file=$1
     output_file=$2
     gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$output_file" "$input_file"
     if [ $? -eq 0 ]; then
-        echo "Kompresi berhasil: $output_file"
+        echo -e "${GREEN}Kompresi berhasil: $output_file${NC}"
     else
-        echo "Kompresi gagal"
+        echo -e "${RED}Gagal Mengkompres PDF${NC}"
     fi
 }
-
-# Fungsi untuk mengunci PDF menggunakan qpdf
+# mengunci PDF menggunakan qpdf
 lock_pdf() {
     input_file=$1
     output_file=$2
     password=$3
     qpdf --encrypt "$password" "$password" 256 -- "$input_file" "$output_file"
     if [ $? -eq 0 ]; then
-        echo "File berhasil dikunci: $output_file"
+        echo -e "${GREEN}File berhasil dikunci: $output_file${NC}"
     else
-        echo "Gagal mengunci file"
+        echo -e "${RED}Gagal Mengunci File${NC}"
     fi
 }
 
-# Fungsi untuk membuka kunci PDF menggunakan qpdf
+# membuka kunci PDF menggunakan qpdf
 unlock_pdf() {
     input_file=$1
     output_file=$2
     password=$3
     qpdf --decrypt --password="$password" "$input_file" "$output_file"
     if [ $? -eq 0 ]; then
-        echo "File berhasil dibuka kuncinya: $output_file"
+        echo -e "${GREEN}File berhasil dibuka kuncinya: $output_file${NC}"
     else
-        echo "Gagal membuka kunci file"
+        echo -e "${RED}Gagal Membuaka Kunci File${NC}"
     fi
 }
 
-# Fungsi untuk mengubah kunci PDF menggunakan qpdf
+# mengubah kunci PDF menggunakan qpdf
 change_pdf_password() {
     input_file=$1
     output_file=$2
@@ -127,23 +136,24 @@ change_pdf_password() {
     new_password=$4
     qpdf --decrypt --password="$old_password" --encrypt "$new_password" "$new_password" 256 -- "$input_file" "$output_file"
     if [ $? -eq 0 ]; then
-        echo "Kunci file berhasil diubah: $output_file"
+        echo -e "${GREEN}Kunci file berhasil diubah: $output_file${NC}"
     else
-        echo "Gagal mengubah kunci file"
+        echo -e "${RED}Gagal Mengubah Kunci File${NC}"
     fi
 }
 
-# Loop utama untuk menu
-echo "====================================="
-echo "============ Dibuat Oleh ============"
-echo "Nama  : Taufik Dimas Edystara"
-echo "NIM   : 2341720062"
-echo "Kelas : TI-1B D-IV Teknik Informatika "
-echo "====================================="
+# program main
+echo "╔══════════════════════════════════════╗"
+echo "║             Dibuat Oleh              ║"
+echo "╠══════════════════════════════════════╣"
+echo "║ Nama  : Taufik Dimas Edystara        ║"
+echo "║ NIM   : 2341720062                   ║"
+echo "║ Kelas : TI-1B D-IV Teknik Informatika║"
+echo "╚══════════════════════════════════════╝"
 while true; do
     show_menu
-    read -p "Masukkan Pilihan (0-12) :" choice
-    case $choice in
+    read -p ">>> Masukkan Pilihan (0-12) :" pilihan
+    case $pilihan in
         1)
             read -p "Masukkan nama file Word: " input_file
             convert_to_pdf "$input_file" "pdf"
@@ -177,7 +187,7 @@ while true; do
             ;;
         8)
             read -p "Masukkan nama file PDF: " input_file
-            read -p "Masukkan pola output file (misal halaman-%d.pdf): " output_pattern
+            read -p "Masukkan pola output file (-%d.pdf): " output_pattern
             split_pdf "$input_file" "$output_pattern"
             ;;
         9)
@@ -213,7 +223,7 @@ while true; do
             exit 0
             ;;
         *)
-            echo "Pilihan tidak valid"
+        echo -e "${RED}Pilihan Tidak Valid${NC}"
             ;;
     esac
 done
